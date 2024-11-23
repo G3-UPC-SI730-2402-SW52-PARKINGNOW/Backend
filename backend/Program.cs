0211a24a0fa712dcf5195d437f59d0eec6ade71c
Diego Ulises Soto Quispe <148501686+Diego2005z@users.noreply.github.com>
2024-11-23 11:16:12 -0500

    update

--- backend/Program.cs
+++ backend/Program.cs
@@ -3,51 +3,62 @@ using backend.Shared.Infrastructure.Interfaces.ASP.Configuration;
 using backend.Shared.Infrastructure.Persistence.EFC.Configuration;
 using backend.Shared.Infrastructure.Persistence.EFC.Repositories;
 using Microsoft.EntityFrameworkCore;
+using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
+
 // Add services to the container.
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at
-https://aka.ms/aspnetcore/swashbuckle
+// https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 // Add Database Connection
-var connectionString =
-    builder.Configuration.GetConnectionString("DefaultConnection");
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
 // Configure Database Context and Logging Levels
 builder.Services.AddDbContext<AppDbContext>(
     options =>
     {
         if (connectionString != null)
+        {
+            var serverVersion = new MySqlServerVersion(new Version(8, 0, 21)); // Ajusta la versión según tu instalación de MySQL
+
             if (builder.Environment.IsDevelopment())
-                options.UseMySQL(connectionString)
+            {
+                options.UseMySql(connectionString, serverVersion)
                     .LogTo(Console.WriteLine, LogLevel.Information)
                     .EnableSensitiveDataLogging()
                     .EnableDetailedErrors();
+            }
             else if (builder.Environment.IsProduction())
-                options.UseMySQL(connectionString)
+            {
+                options.UseMySql(connectionString, serverVersion)
                     .LogTo(Console.WriteLine, LogLevel.Error)
                     .EnableSensitiveDataLogging()
                     .EnableDetailedErrors();
+            }
+        }
     });
+
 // Configure Dependency Injection
 // Shared Bounded Context Injection Configuration
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
 var app = builder.Build();
-// Verify Database Objects are created
-using (var scope = app.Services.CreateScope())
-{
-    var services = scope.ServiceProvider;
-    var context = services.GetRequiredService<AppDbContext>();
-    context.Database.EnsureCreated();
-}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
 app.UseHttpsRedirection();
+
 app.UseAuthorization();
+
 app.MapControllers();
+
 app.Run();
